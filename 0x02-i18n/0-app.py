de558491575e5c7d560b5e48d1e08d5580854e94
OmarKHDR <omarsliman37@gmail.com>
2024-11-06 11:26:31 +0200

    pys help

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+'''Hello world is documented
+'''
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
+    '''Hello world is documented
+    '''
     return render_template('0-index.html')
