d71e8aa981af7208cae0829c72a1509590d08b10
OmarKHDR <omarsliman37@gmail.com>
2024-11-06 15:52:46 +0200

    not a fan but it worked

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 '''Hello world
 '''
-from app import babel, app
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    '''This is docs
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/')
