1512cb08c82b68b9a198edffdcb7f5408952a2f9
OmarKHDR <omarsliman37@gmail.com>
2024-11-25 14:33:18 +0200

    fwe

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''hell is world
+'''
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        if key == None or item == None:
+            return 
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
