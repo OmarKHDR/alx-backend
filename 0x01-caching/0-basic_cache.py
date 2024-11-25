5730da4a83555ee5615f403885667561af6e3cf0
OmarKHDR <omarsliman37@gmail.com>
2024-11-25 14:35:38 +0200

    fwe

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,11 +8,15 @@ class BasicCache(BaseCaching):
     '''This is why not how
     '''
     def put(self, key, item):
+        '''This wasnt documented
+        '''
         if key == None or item == None:
             return 
         self.cache_data[key] = item
 
     def get(self, key):
+        '''Neither this was that si lfei
+        '''
         if key in self.cache_data:
             return self.cache_data[key]
         else:
