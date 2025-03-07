3832ef9667ce04038c45ac7dc164d738c40b9e9b
OmarKHDR <omarsliman37@gmail.com>
2025-03-07 14:32:10 +0200

    done, next

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """File docs is here"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 from babel import Locale
 import pytz
 
@@ -17,15 +17,15 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+babel.init_app(app, locale_selector=get_locale)
 
 @app.route('/')
 def home():
     """Home function route"""
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
