94bd83886e652e9ecc594566cfec3a34a161d113
OmarKHDR <omarsliman37@gmail.com>
2025-03-07 15:12:12 +0200

    jhellow w fnpw

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""File docs is here"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+from babel import Locale
+import pytz
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = "en"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale():
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+babel.init_app(app, locale_selector=get_locale)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """Home function route"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    """Module level docs"""
+    app.run()
